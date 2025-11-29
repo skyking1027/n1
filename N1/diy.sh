b3bb0d5db0d8df2ec5f8e768fd29ee93ddca2d80
skyking1027 <61034437+skyking1027@users.noreply.github.com>
2025-11-30 06:23:14 +0800

    Update diy.sh

--- N1/diy.sh
+++ N1/diy.sh
@@ -6,8 +6,8 @@
 
 # Add packages
 git clone --depth=1 https://github.com/ophub/luci-app-amlogic package/amlogic
-git clone  https://github.com/linkease/nas-packages-luci/luci/luci-app-linkease.git package/luci-app-linkease
-git clone  https://github.com/linkease/nas-packages/network/services/linkease.git package/linkease
+git clone  https://github.com/linkease/nas-packages-luci.git luci/luci-app-linkease
+git clone  https://github.com/linkease/nas-packages.git network/services/linkease
 #git clone --depth=1 https://github.com/kiddin9/kwrt-packages.git package/luci-app-linkease
 #git clone --depth=1 https://github.com/kiddin9/kwrt-packages.git package/linkease
 
