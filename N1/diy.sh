c207a7270d41b5a7d35b63c8a70c7935423bf245
skyking1027 <61034437+skyking1027@users.noreply.github.com>
2025-12-01 05:12:59 +0800

    Update diy.sh

--- N1/diy.sh
+++ N1/diy.sh
@@ -6,12 +6,9 @@
 
 # Add packages
 git clone --depth=1 https://github.com/ophub/luci-app-amlogic package/amlogic
-#git clone  https://github.com/linkease/nas-packages-luci.git luci/luci-app-linkease
-#git clone  https://github.com/linkease/nas-packages.git network/services/linkease
 #git clone --depth=1 https://github.com/kiddin9/kwrt-packages.git package/luci-app-linkease
 #git clone --depth=1 https://github.com/kiddin9/kwrt-packages.git package/linkease
-git clone https://github.com/linkease/nas-packages-luci.git package/nas-packages-luci
-git clone https://github.com/linkease/nas-packages.git package/nas-packages
+git clone --depth=1 https://github.com/kiddin9/kwrt-packages package/luci-app-linkease
 git clone --depth=1 https://github.com/nikkinikki-org/OpenWrt-nikki package/luci-app-nikki
 
 # 加入OpenClash核心
@@ -22,7 +19,7 @@ sed -i '$a src-git kiddin9_openwrt_packages https://github.com/kiddin9/kwrt-pack
 echo "
 # 插件
 CONFIG_PACKAGE_luci-app-amlogic=y
-CONFIG_PACKAGE_luci-app-linkease=y
+#CONFIG_PACKAGE_luci-app-linkease=y
 CONFIG_PACKAGE_luci-app-nikki=y
 " >> .config
 
